e89e2b48bf8a8fd783c853baa710cd04c315a48e
Obedia Lawrence <omlawrence24@gmail.com>
2021-05-07 17:36:56 -0400

    employee

--- my-react-app/src/components/table/index.js
+++ my-react-app/src/components/table/index.js
@@ -1,38 +1,30 @@
 import React from "react";
-//import {Employee} from "../../pages/Employee";
-
-
-
-
+import Employee from "../../pages/Employee";
 // this component handles the employees table
 function Table(props) {
   return (
-  <div>
+    <div>
       <table class="table table-bordered border-primary">
         <thead>
           <tr>
-         
-            <th 
-           onClick ={ ()=>{props.sortHandler('First Name')}}
-            scope="col">
-              First Name</th>
+            <th scope="col"></th>
+            <th
+              onClick={() => {
+                props.handleSort();
+              }}
+              scope="col"
+            >
+              First Name
+            </th>
             <th scope="col"> Last Name</th>
             <th scope="col">Phone Number</th>
-            <th onClick ={ ()=>{props.sortHandler('Age')}}
-             scope="col">
-               Age</th>
+            <th scope="col">Age</th>
             <th scope="col">Email</th>
           </tr>
         </thead>
-
-        <tbody>
-         {props.children}
-        </tbody>
-
+        <tbody>{props.children}</tbody>
       </table>
-      </div>
-  )
-
- };
-
-export default Table;    
+    </div>
+  );
+}
+export default Table;
--- my-react-app/src/pages/Employee.js
+++ my-react-app/src/pages/Employee.js
@@ -2,122 +2,86 @@ import { Component } from "react";
 import API from "../utils/API";
 import Table from "../components/Table";
 import Row from "../components/Row";
-
 class Employee extends Component {
   state = {
     search: "",
     employee: [],
     filter: [],
-    sort: true,
-    error: ""
+    sort: "desc",
+    error: "",
   };
-  headings = [
-    { name: "First Name" },
-    { name: "Age" }
-  ]
-
-  // let props = {firstName, lastName, phone, age, email}  
-
+  headings = [{ name: "First Name" }, { name: "Age" }];
+  // let props = {firstName, lastName, phone, age, email}
   // When the component mounts, get a list of all employees
   componentDidMount() {
-    API.getRandomEmployees()
-      .then(res => this.setState({ employee: res.data.results }))
-    console.log(this.state.employee)
-
+    API.getRandomEmployees().then((res) =>
+      this.setState({ employee: res.data.results })
+    );
+    console.log(this.state.employee);
   }
-
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-    if (event.target.value === "First Name") {
-    
-    } if (event.target.value === "Age") {
-
-    }
+  handleSort = (event) => {
+    console.log("sort by name");
+    this.sortEmployees();
   };
-
-  sortHandler(heading) {
-    if (this.state.order === false) {
-      this.setState({
-        order: true
-      }
-      )
-    }
-    else this.setState({
-      order: false
-    }
-    )
-
-    const makeAlphabetical = (a, b) => {
-
-      if (this.state.order === false) {
-        if (a[heading] === "First Name") {
-          return a[heading].first.localeCompare(b[heading].first)
-
-        } else if (a[heading] === "Age") {
-          return a[heading].first.localeCompare(b[heading].first)
-        }
-
-      }
-    }
-    console.log(this.state)
-    const sortedUsers = this.state.employee.sort(makeAlphabetical)
-    this.setState({
-      employee: sortedUsers
-    })
-  }
-  handleFormSubmit = event => {
+  sortEmployees = () => {
+    let employee = this.state.employee;
+    employee.sort(function (a, b) {
+      return a.name.first > b.name.first ? 1 : -1;
+    });
+    return this.setState({
+      employee: employee,
+    });
+  };
+  handleFormSubmit = (event) => {
     event.preventDefault();
     try {
-      API.getRandomEmployees(this.state.search)
-        .then(res => {
-          if (res.data.status === "error") {
-            throw new Error(res.data.message);
-          }
-          this.setState({ results: res.data.message, error: "" });
-        })
+      API.getRandomEmployees(this.state.search).then((res) => {
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        this.setState({ results: res.data.message, error: "" });
+      });
+    } catch (error) {
+      this.setState({ error });
     }
-    catch (error) {
-      this.setState({ error })
-    }
-
   };
-
   render() {
-    console.log(this.state.employee)
     return (
       <div>
         <h1> Employee List </h1>
-
         <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">Search</span>
-          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-          </input>
+          <span class="input-group-text" id="inputGroup-sizing-default">
+            Search
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default"
+          ></input>
         </div>
-
         <div className="card text-center">
           <div className="card-header">
             <div>
-
-              <Table sortHandler = {this.sortHandler}>
-                
-                {this.state.employee.length > 0 && this.state.employee.map(data => (
-                  <Row
-                  //  key={employee._id} 
-                    firstName={data.name.first}
-                    lastName={data.name.last}
-                    age={data.dob.age}
-                    phone={data.phone}
-                    email={data.email}
-                  />
-                ))}
+              <Table handleSort={this.handleSort}>
+                {this.state.employee.length > 0 &&
+                  this.state.employee.map((data) => (
+                    <Row
+                      employees={this.filter}
+                      firstName={data.name.first}
+                      lastName={data.name.last}
+                      age={data.dob.age}
+                      phone={data.phone}
+                      email={data.email}
+                      sortHandler={this.sortHandler}
+                    />
+                  ))}
               </Table>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
-
-};
-
+}
 export default Employee;
